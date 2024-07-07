5a526b029f19d0b3099c5835f5216a8cda2f114b
Tomasz Oszczypko <tomekoszczypko@gmail.com>
2024-07-07 13:51:01 +0200

    Changed if statement to assert in ESM and added comments

--- Scenes/EnemyStateMachine/enemy_state_machine.gd
+++ Scenes/EnemyStateMachine/enemy_state_machine.gd
@@ -1,16 +1,20 @@
+# Enemy State Machine used for enemies' AI.
+
 class_name EnemyStateMachine
 extends Node
 
-@export var _state: State
+@export var _state: State  # default/active state
 
 
+# Enables default state.
 func _ready() -> void:
 	assert(_state != null)
 	change_state(_state)
 
 
+# Changes states by disabling old one and enabling new one.
 func change_state(new_state: State) -> void:
-	if _state is State:  # TODO: this check might not be necessary
-		_state.exit_state()
+	assert(_state is State)  # makes sure none of us forgets about setting default state
+	_state.exit_state()
 	new_state.enter_state()
 	_state = new_state
