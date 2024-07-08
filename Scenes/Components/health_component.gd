f548447135cb7968608ab53609b3f8f533d99542
Tomasz Oszczypko <tomekoszczypko@gmail.com>
2024-07-08 18:36:06 +0200

    Added damage indicators

--- Scenes/Components/health_component.gd
+++ Scenes/Components/health_component.gd
@@ -3,6 +3,9 @@
 # Note that for proper working it needs some sort of collision checking to apply damage, so it is
 # used as HurtboxComponent's export variable.
 #
+# After applying damage, it spawns damage indicator in its global_position, so make sure to posiiton
+# it in the entity scene.
+#
 # Hurt effect or death should be implemented in entity's script using available signals.
 #
 # max_health and health are public variables, so they can be used externally.
@@ -14,6 +17,8 @@ extends Node2D
 signal health_changed  # emitted when damage is applied
 signal health_depleted  # emitted when lethal amount damage is applied
 
+const DamageIndicatorScene := preload("res://Scenes/UI/damage_indicator.tscn")
+
 @export var max_health: int
 
 var health: int
@@ -24,8 +29,18 @@ func _ready() -> void:
 
 
 # Reduces entity's damage by amount passed as argument and emits signals.
+# Also spawns damage indicators with applied damage.
 func apply_damage(amount: int) -> void:
 	health -= amount
 	health_changed.emit()
 	if health <= 0:
 		health_depleted.emit()
+	_spawn_damage_indicator(amount)
+
+
+# Spawns animated damage indicator at the component's position.
+func _spawn_damage_indicator(damage: int) -> void:
+	var _indicator := DamageIndicatorScene.instantiate()
+	_indicator.text = str(damage)
+	_indicator.global_position += global_position
+	get_tree().root.add_child(_indicator)
---
+++ Scenes/UI/damage_indicator.gd
@@ -0,0 +1,11 @@
+# Label that indicates applied damage. Used by HealthComponent.
+# When ready, it starts a short animation that moves itself by 16 pixels to up, then deletes itself.
+
+extends Label
+
+
+func _ready() -> void:
+	var _tween := get_tree().create_tween()
+	_tween.tween_property(self, "position", Vector2(0.0, -16.0), 1).as_relative() \
+			.set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
+	_tween.tween_callback(queue_free)
