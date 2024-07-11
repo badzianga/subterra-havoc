c20a2b19cda701c8ef3a7bad8e4b330bd2e1dd2d
Tomasz Oszczypko <tomekoszczypko@gmail.com>
2024-07-11 22:51:39 +0200

    Added commented dash screen shake and air resistance

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -5,6 +5,7 @@ const SPEED := 300.0
 const JUMP_VELOCITY := -400.0
 const CUT_JUMP_HEIGHT := 0.4
 const DASH_MULTIPLIER := 2.5
+#const AIR_RESISTANCE := 10.0
 
 var _gravity := ProjectSettings.get_setting("physics/2d/default_gravity") as float
 var _direction := Vector2.ZERO
@@ -12,6 +13,12 @@ var _direction := Vector2.ZERO
 var _can_dash := true
 var _is_dashing := false
 var _dash_direction: Vector2
+
+#var _random_strength := 4.0
+#var _shake_fade := 12.0
+#var _rng := RandomNumberGenerator.new()
+#var _shake_strength := 0.0
+#var _prev_velocity: Vector2
 
 @onready var _health_component := $HealthComponent as HealthComponent
 @onready var _health_bar := $UserInterface/HealthBar
@@ -24,6 +31,7 @@ var _dash_direction: Vector2
 @onready var _hurtbox_collider := $HurtboxComponent/CollisionShape
 @onready var _immunity_frames_timer := $ImmunityFramesTimer
 @onready var _blinking_animation := $ImmunityFramesTimer/BlinkingAnimation
+#@onready var _camera := $Camera
 
 
 func _ready() -> void:
@@ -81,7 +89,19 @@ func _handle_movement(delta: float) -> void:
 		# do that if we implement accelerating
 		velocity.x = move_toward(velocity.x, 0.0, SPEED)
 	
+	 #air resistance
+	#if not is_on_floor() and not _is_dashing:
+		#velocity.x = lerp(_prev_velocity.x, velocity.x, AIR_RESISTANCE * delta)
+	
 	move_and_slide()
+	
+	# also used in air resistance
+	#_prev_velocity.x = velocity.x
+	
+	# camera shake when dashing
+	#if _shake_strength > 0.0:
+		#_shake_strength = lerpf(_shake_strength, 0, _shake_fade * delta)
+		#_camera.offset = _random_offset()
 
 
 func _handle_animations() -> void:
@@ -117,6 +137,18 @@ func _check_dashing() -> void:
 		_dashing_timer.start()
 		# dashing while jumping makes player jump higher, multiplying velocity.y reduces this height
 		velocity.y *= 0.3
+		#_apply_shake()
+
+
+#func _apply_shake() -> void:
+	#_shake_strength = _random_strength
+#
+#
+#func _random_offset() -> Vector2:
+	#return Vector2(
+		#_rng.randf_range(-_shake_strength, _shake_strength),
+		#_rng.randf_range(-_shake_strength, _shake_strength)
+	#)
 
 
 func _on_health_component_health_changed() -> void:
