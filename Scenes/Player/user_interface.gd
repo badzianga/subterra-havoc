a5db3188ac6fae1a9672e9ef07cf846806ad69a7
Tomasz Oszczypko <tomekoszczypko@gmail.com>
2024-07-13 00:29:47 +0200

    Added UserInterface to GlobalVariables singleton

---
+++ Scenes/Player/user_interface.gd
@@ -0,0 +1,6 @@
+class_name UserInterface
+extends CanvasLayer
+
+
+func _ready() -> void:
+	GlobalVariables.user_interface_node = self
--- Singletons/global_variables.gd
+++ Singletons/global_variables.gd
@@ -1,6 +1,7 @@
 extends Node
 
 var player: Player
+var user_interface_node: UserInterface
 
 
 func clear_all() -> void:
