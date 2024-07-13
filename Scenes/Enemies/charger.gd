8fcbfb1327fdb3fd02ddd746344378495f4a8a4a
Tomasz Oszczypko <tomekoszczypko@gmail.com>
2024-07-14 00:09:43 +0200

    Added prototype melee attack and killing enemies

--- Scenes/Enemies/charger.gd
+++ Scenes/Enemies/charger.gd
@@ -1,31 +1,34 @@
 extends Enemy
 
-@onready var esm := $EnemyStateMachine as EnemyStateMachine
-@onready var idle_state := $EnemyStateMachine/IdleState as IdleState
-@onready var wander_state := $EnemyStateMachine/WanderState as WanderState
-@onready var prepare_state := $EnemyStateMachine/PrepareState as PrepareState
-@onready var charge_state := $EnemyStateMachine/ChargeState as ChargeState
-@onready var cooldown_state := $EnemyStateMachine/CooldownState as CooldownState
+@onready var _esm := $EnemyStateMachine as EnemyStateMachine
+@onready var _idle_state := $EnemyStateMachine/IdleState as IdleState
+@onready var _wander_state := $EnemyStateMachine/WanderState as WanderState
+@onready var _prepare_state := $EnemyStateMachine/PrepareState as PrepareState
+@onready var _charge_state := $EnemyStateMachine/ChargeState as ChargeState
+@onready var _cooldown_state := $EnemyStateMachine/CooldownState as CooldownState
+@onready var _health_component := $HealthComponent as HealthComponent
 
 
 func _ready() -> void:
 	_connect_states()
+	
+	_health_component.health_depleted.connect(_on_health_depleted)
 
 
 func _connect_states() -> void:
-	idle_state.idling_finished.connect(esm.change_state.bind(wander_state))
-	idle_state.player_seen.connect(esm.change_state.bind(prepare_state))
+	_idle_state.idling_finished.connect(_esm.change_state.bind(_wander_state))
+	_idle_state.player_seen.connect(_esm.change_state.bind(_prepare_state))
 	
-	wander_state.wandering_finished.connect(esm.change_state.bind(idle_state))
-	wander_state.player_seen.connect(esm.change_state.bind(prepare_state))
+	_wander_state.wandering_finished.connect(_esm.change_state.bind(_idle_state))
+	_wander_state.player_seen.connect(_esm.change_state.bind(_prepare_state))
 	
-	prepare_state.player_lost.connect(esm.change_state.bind(cooldown_state))
-	prepare_state.preparing_finished.connect(esm.change_state.bind(charge_state))
+	_prepare_state.player_lost.connect(_esm.change_state.bind(_cooldown_state))
+	_prepare_state.preparing_finished.connect(_esm.change_state.bind(_charge_state))
 	
-	charge_state.charging_finished.connect(esm.change_state.bind(cooldown_state))
+	_charge_state.charging_finished.connect(_esm.change_state.bind(_cooldown_state))
 	
-	cooldown_state.cooldown_finished.connect(esm.change_state.bind(wander_state))
-	cooldown_state.player_seen.connect(esm.change_state.bind(prepare_state))
+	_cooldown_state.cooldown_finished.connect(_esm.change_state.bind(_wander_state))
+	_cooldown_state.player_seen.connect(_esm.change_state.bind(_prepare_state))
 
 
 func _on_detection_area_area_entered(_area: Area2D) -> void:
@@ -34,3 +37,7 @@ func _on_detection_area_area_entered(_area: Area2D) -> void:
 
 func _on_detection_area_area_exited(_area: Area2D) -> void:
 	player_in_detection_area = false
+
+
+func _on_health_depleted() -> void:
+	queue_free()
--- Scenes/Weapons/bronze_spear.gd
+++ Scenes/Weapons/bronze_spear.gd
@@ -1,7 +1,17 @@
 extends Sprite2D
 
 @onready var _animation_player := $AnimationPlayer
+@onready var _hitbox_collider := $HitboxComponent/CollisionShape
 
 
 func attack() -> void:
 	_animation_player.play("attack")
+	await _animation_player.animation_finished
+
+
+func _enable_collider() -> void:
+	_hitbox_collider.set_deferred("disabled", false)
+
+
+func _disable_collider() -> void:
+	_hitbox_collider.set_deferred("disabled", true)
