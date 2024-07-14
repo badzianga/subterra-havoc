5c460c02f8e84bedae8eb64d4e4d348c5344f19d
Tomasz Oszczypko <tomekoszczypko@gmail.com>
2024-07-14 14:49:23 +0200

    Added world node to global variables

---
+++ Scenes/Maps/world.gd
@@ -0,0 +1,5 @@
+extends Node2D
+
+
+func _ready() -> void:
+	GlobalVariables.world_node = self
--- Singletons/global_variables.gd
+++ Singletons/global_variables.gd
@@ -3,7 +3,11 @@ extends Node
 var player: Player
 var user_interface_node: UserInterface
 var inventory_node: Inventory
+var world_node: Node2D
 
 
 func clear_all() -> void:
 	player = null
+	user_interface_node = null
+	inventory_node = null
+	world_node = null
