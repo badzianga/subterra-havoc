1f16b9d3539598849d17dd5f4dfae14fd9ed65ed
Tomasz Oszczypko <tomekoszczypko@gmail.com>
2024-07-19 23:16:15 +0200

    Added arrow deleting when collide with world

--- Scenes/Weapons/wooden_arrow.gd
+++ Scenes/Weapons/wooden_arrow.gd
@@ -12,3 +12,7 @@ func _physics_process(delta: float) -> void:
 
 func _on_area_entered(_area: Area2D) -> void:
 	call_deferred("queue_free")
+
+
+func _on_body_entered(_body: Node2D) -> void:
+	queue_free()
