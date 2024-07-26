a456e61fee7950267fb2e57cc78062c031f5266b
Tomasz Oszczypko <tomekoszczypko@gmail.com>
2024-07-26 20:40:56 +0200

    Added simple map switching

---
+++ Scenes/MapSwitching/portal.gd
@@ -0,0 +1,17 @@
+# FIXME: player body is not on World layer, so checking PlayerHurtbox
+# however, dashing temporarily disables hurtbox, so it will cause bugs later
+
+class_name Portal
+extends Area2D
+
+@export_file("*.tscn") var _next_map_path: String
+
+
+func _ready() -> void:
+	assert(not _next_map_path.is_empty())
+
+
+func _on_area_entered(_area: Area2D) -> void:
+	# TODO: this is bad; changing scene should be handled by singleton
+	var _next_map_scene := load(_next_map_path) as PackedScene
+	get_tree().call_deferred("change_scene_to_packed", _next_map_scene)
