03d3f0c1b7fa91851dfa789450a76015b96182d7
Tomasz Oszczypko <tomekoszczypko@gmail.com>
2024-08-04 14:21:23 +0200

    Added very basic main menu

---
+++ Scenes/Menu/main_menu.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+func _on_start_button_pressed() -> void:
+	pass
+
+
+func _on_settings_button_pressed() -> void:
+	pass
+
+
+func _on_exit_button_pressed() -> void:
+	get_tree().quit()
