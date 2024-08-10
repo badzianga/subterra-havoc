91b88c0133d2dbb96ab0b25454c5e24e5245a3e2
Tomasz Oszczypko <tomekoszczypko@gmail.com>
2024-08-11 00:20:14 +0200

    Added comments to logging singleton

--- Singletons/logger.gd
+++ Singletons/logger.gd
@@ -1,3 +1,7 @@
+# Logging singleton which enables better console outputs with timestamps and
+# log levels. Note that logs don't pause or exit game - they only display
+# messages with severities, enabling better readability and filtering. 
+
 extends Node
 
 enum LogLevel {
@@ -7,33 +11,42 @@ enum LogLevel {
 	ERROR,
 }
 
+const COLORS: Array[String] = ["gray", "white", "yellow", "red"]
+
 var log_level := LogLevel.DEBUG
 
 
+# Displays lowest severity message for debugging purposes.
 func debug(message: String) -> void:
-	_log(LogLevel.DEBUG, message, "gray")
+	_log(LogLevel.DEBUG, message)
 
 
+# Displays informational logs - little brighter than debug messages.
 func info(message: String) -> void:
-	_log(LogLevel.INFO, message, "white")
+	_log(LogLevel.INFO, message)
 
 
+# Displays warning messages for non-critical behaviors.
 func warning(message: String) -> void:
-	_log(LogLevel.WARNING, message, "yellow")
+	_log(LogLevel.WARNING, message)
 
 
+# Displays important errors which can even be critical.
 func error(message: String) -> void:
-	_log(LogLevel.ERROR, message, "red")
+	_log(LogLevel.ERROR, message)
 
 
-func _log(level: LogLevel, message: String, color: String) -> void:
+# Logging function used by public functions, which filters logging level and
+# displays string in proper color with time, level and message.
+func _log(level: LogLevel, message: String) -> void:
 	if log_level > level:
 		return
 	var time_str := _get_time_string()
 	var level_name := LogLevel.keys()[level] as String
-	print_rich("[color=%s][%s] [%s] %s[/color]" % [color, time_str, level_name, message])
+	print_rich("[color=%s][%s] [%s] %s[/color]" % [COLORS[level], time_str, level_name, message])
 
 
+# Returns time string with milliseconds.
 func _get_time_string() -> String:
 	var ms := int(fposmod(Time.get_unix_time_from_system(), 1.0) * 1000.0)
 	return "%s.%d" % [Time.get_time_string_from_system(), ms]
