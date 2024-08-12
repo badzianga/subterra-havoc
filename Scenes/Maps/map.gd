95b9394eb5539afaf4c7657d9b59b23f67a0df67
Tomasz Oszczypko <tomekoszczypko@gmail.com>
2024-08-12 20:18:37 +0200

    Changed map debug label to log info

--- Scenes/Maps/map.gd
+++ Scenes/Maps/map.gd
@@ -26,10 +26,4 @@ func _ready() -> void:
 			make sure to load everything"
 		)
 	
-	# TEMPORARY FOR DEBUG PURPOSES
-	var _label: Label = $CanvasLayer/DebugLabel
-	_label.text = "Name: " + name + ", type: " + MapType.keys()[map_type]
-	await get_tree().create_timer(3.0).timeout
-	var _tween := create_tween()
-	_tween.tween_property(_label, "modulate", Color(255, 255, 255, 0), 1.0)
-	_tween.tween_callback(_label.queue_free)
+	Logger.info("Current map name: %s, type: %s" % [name, MapType.keys()[map_type]])
