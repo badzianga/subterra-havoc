f8b7e514bb8453fb9dd8dc984f7472ae1dc300d5
Tomasz Oszczypko <tomekoszczypko@gmail.com>
2024-11-03 17:32:25 +0100

    Base code for next enemy

---
+++ Scenes/Enemies/MossBiome/mossive_max.gd
@@ -0,0 +1,32 @@
+# TODO: now I'm raping DRY principle, so i think it would be a good idea to
+# create function in enemy class for easy state connecting by using dictionary
+# or something like that - but how to use different signals? Maybe by using
+# .get (I think it was named like that)
+extends Enemy
+
+@onready var esm := $EnemyStateMachine as EnemyStateMachine
+@onready var idle := $EnemyStateMachine/IdleState as IdleState
+@onready var wander := $EnemyStateMachine/WanderState as WanderState
+@onready var prepare := $EnemyStateMachine/PrepareState as PrepareState
+@onready var cooldown := $EnemyStateMachine/CooldownState as CooldownState
+@onready var attack := $EnemyStateMachine/AttackState as AttackState
+
+
+func _ready() -> void:
+	idle.idling_finished.connect(esm.change_state.bind(wander))
+	idle.player_seen.connect(esm.change_state.bind(prepare))
+	
+	wander.player_seen.connect(esm.change_state.bind(prepare))
+	wander.wandering_finished.connect(esm.change_state.bind(idle))
+	
+	prepare.player_lost.connect(esm.change_state.bind(idle))
+	prepare.preparing_finished.connect(esm.change_state.bind(attack))
+
+	attack.attacking_finished.connect(esm.change_state.bind(cooldown))
+	
+	cooldown.cooldown_finished.connect(esm.change_state.bind(idle))
+	cooldown.player_seen.connect(esm.change_state.bind(prepare))
+
+
+func _on_health_depleted() -> void:
+	queue_free()
---
+++ Scenes/EnemyStateMachine/attack_state.gd
@@ -0,0 +1,6 @@
+class_name AttackState
+extends State
+
+signal attacking_finished
+
+# TODO: attacking is not started nor finished
