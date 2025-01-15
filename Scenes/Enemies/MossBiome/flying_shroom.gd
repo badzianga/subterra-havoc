6567575e4acd92ba00595da7b9cf985b37e9c173
Tomasz Oszczypko <tomekoszczypko@gmail.com>
2025-01-15 14:07:39 +0100

    Added multiple mushrooms with flying shroom attack

--- Scenes/Enemies/MossBiome/flying_shroom.gd
+++ Scenes/Enemies/MossBiome/flying_shroom.gd
@@ -13,6 +13,12 @@ const FungalSporeScene := preload("res://Scenes/Enemies/Attacks/fungal_spore.tsc
 @onready var detection_area := $DetectionArea
 @onready var vision_cast := $VisionCast
 
+const angles: Array[Array] = [
+	[0.0],
+	[deg_to_rad(-10.0), deg_to_rad(10.0)],
+	[deg_to_rad(-20.0), 0.0, deg_to_rad(20.0)]
+]
+
 
 func _ready() -> void:
 	idle.idling_finished.connect(esm.change_state.bind(wander))
@@ -41,13 +47,15 @@ func flip(flip_to_right: bool) -> void:
 # TODO: create more spores, maybe randomize amount of spores - 1-3 per attack
 # also add proper bullet dispersion
 func _attack() -> void:
-	var fungal_spore := FungalSporeScene.instantiate()
-	fungal_spore.global_position = global_position
-	var dir := global_position.direction_to(GlobalVariables.player.global_position)
-	var angle := dir.angle() + randf_range(-0.1, 0.1)
-	fungal_spore.direction = Vector2.from_angle(angle)
-	fungal_spore.rotation = angle
-	GlobalVariables.map_node.add_child(fungal_spore)
+	var amount := randi_range(1, 3)
+	for i in range(amount):
+		var fungal_spore := FungalSporeScene.instantiate()
+		fungal_spore.global_position = global_position
+		var dir := global_position.direction_to(GlobalVariables.player.global_position)
+		var angle := dir.angle() + randf_range(-0.1, 0.1) + (angles[amount - 1][i] as float)
+		fungal_spore.direction = Vector2.from_angle(angle)
+		fungal_spore.rotation = angle
+		GlobalVariables.map_node.add_child(fungal_spore)
 
 
 func _on_detection_area_area_entered(_area: Area2D) -> void:
